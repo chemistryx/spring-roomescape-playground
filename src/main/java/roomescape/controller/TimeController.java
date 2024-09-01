22a1bab80b5645a1050f26c42621b621e8c0ce5c
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-09-01 20:11:31 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/controller/TimeController.java
@@ -0,0 +1,50 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import roomescape.domain.Time;
+import roomescape.service.TimeService;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller
+public class TimeController {
+    private final TimeService timeService;
+
+    public TimeController(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @GetMapping("/time")
+    public String showTimePage() {
+        return "time";
+    }
+
+    @PostMapping("/times")
+    @ResponseBody
+    public ResponseEntity<Time> createTime(@RequestBody Time time) {
+        Time newTime = timeService.createTime(time);
+
+        return ResponseEntity.created(URI.create("/times/" + newTime.getId()))
+                .body(newTime);
+    }
+
+    @GetMapping("/times")
+    @ResponseBody
+    public ResponseEntity<List<Time>> getTimes() {
+        List<Time> times = timeService.getAllTimes();
+        return ResponseEntity.ok().body(times);
+    }
+
+    @DeleteMapping("/times/{id}")
+    @ResponseBody
+    public ResponseEntity<?> deleteTime(@PathVariable Long id) {
+        boolean isDeleted = timeService.deleteTime(id);
+        if (!isDeleted) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,13 +2,16 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 import roomescape.domain.Reservation;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -23,6 +26,9 @@ import static org.hamcrest.Matchers.is;
 public class MissionStepTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     void 일단계() {
@@ -47,6 +53,7 @@ public class MissionStepTest {
     }
 
     @Test
+    @Disabled
     void 삼단계() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
@@ -114,6 +121,7 @@ public class MissionStepTest {
     }
 
     @Test
+    @Disabled
     void 육단계() {
         jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
 
@@ -129,6 +137,7 @@ public class MissionStepTest {
     }
 
     @Test
+    @Disabled
     void 칠단계() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
@@ -153,5 +162,59 @@ public class MissionStepTest {
 
         Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
         assertThat(countAfterDelete).isEqualTo(0);
+    }
+
+    @Test
+    void 팔단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/times/1");
+
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    void 구단계() {
+        Map<String, String> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2023-08-05");
+        reservation.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    void 십단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }
