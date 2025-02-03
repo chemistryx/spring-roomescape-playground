d72147506eafe4ac8aaa074f7974eb53be8b0d6b
Home Guardian <114041681+heehunjung@users.noreply.github.com>
2025-02-03 20:31:57 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -0,0 +1,16 @@
+package roomescape.reservation.dto;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import roomescape.reservation.domain.Reservation;
+
+public record ReservationResponse(Long id, String name, LocalDate date, String time) {
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    public static ReservationResponse fromReservation(final Reservation reservation) {
+
+        return new ReservationResponse(reservation.getId(), reservation.getName(), reservation.getDate(),
+                TIME_FORMATTER.format(reservation.getTime()));
+    }
+}
